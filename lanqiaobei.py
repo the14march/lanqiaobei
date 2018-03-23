8e091250ef650eafe537e627cf4c3055588fd9f3
the14march <niekai1995@live.com>
2018-03-23 11:01:03 +0800

    使用session，并添加了错误处理

--- lanqiaobei.py
+++ lanqiaobei.py
@@ -1,16 +1,17 @@
 import os
 import time
-from flask import Flask, request, redirect, url_for, send_from_directory, render_template
+from flask import Flask, request, redirect, url_for, send_from_directory, render_template, g, session
 from flask_script import Manager
 from werkzeug.utils import secure_filename
 
 from utils import get_similar_images
 
 UPLOAD_FOLDER = 'uploads'
-ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+ALLOWED_EXTENSIONS = {'jpg', 'jpeg'}
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.secret_key = "a secret key"
 manager = Manager(app)
 
 
@@ -29,8 +30,22 @@ def upload_file():
             os.makedirs(os.path.join(os.path.dirname(__file__), app.config['UPLOAD_FOLDER'], str(timestamp)))
             file.save(save_path)
             return_list = get_similar_images(os.path.dirname(save_path))
-            return render_template("return_images.html", return_list=return_list)
+            session["return_list"] = return_list
+            return redirect(url_for('result_handler'))
+        else:
+            session["filename"] = file.filename
+            return redirect(url_for('error_handler'))
     return render_template("upload_file.html")
+
+
+@app.route('/results')
+def result_handler():
+    return render_template("return_images.html", return_list=session["return_list"])
+
+
+@app.route('/error')
+def error_handler():
+    return render_template("error.html", filename=session["filename"])
 
 
 @app.route('/image/<filename>')
